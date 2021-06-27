3f5f348fd3dbacc27535e800891a7581e263ad81
Hiromu OCHIAI <otiai10@gmail.com>
2021-06-27 23:48:55 +0900

    Update cron to fetch slack members

--- controllers/crontask.go
+++ controllers/crontask.go
@@ -6,53 +6,9 @@ import (
 	"net/http"
 	"os"
 
+	"cloud.google.com/go/datastore"
 	"github.com/otiai10/marmoset"
-)
-
-type (
-	SlackProfile struct {
-		Title string `json:"title"`
-		// 名前
-		RealName              string `json:"real_name"`
-		RealNameNormalized    string `json:"real_name_normalized"`
-		DisplayName           string `json:"display_name"`
-		DisplayNameNormalized string `json:"display_name_normalized"`
-		FirstName             string `json:"first_name"`
-		LastName              string `json:"last_name"`
-		// アイコン
-		AvatarHash    string `json:"avatar_hash"`
-		IsCustomImage bool   `json:"is_custom_image"`
-		ImageOriginal string `json:"image_original"`
-		Image512      string `json:"image_512"`
-		Team          string `json:"team"`
-	}
-	SlackMember struct {
-		ID                string       `json:"id"`
-		TeamID            string       `json:"team_id"`
-		Name              string       `json:"name"`
-		RealName          string       `json:"real_name"`
-		Deleted           bool         `json:"deleted"`
-		Color             string       `json:"color"`
-		Profile           SlackProfile `json:"profile"`
-		IsAdmin           bool         `json:"is_admin"`
-		IsOwner           bool         `json:"is_owner"`
-		IsPrimaryOwner    bool         `json:"is_primary_owner"`
-		IsRestricted      bool         `json:"is_restricted"`
-		IsUltraRestricted bool         `json:"is_ultra_restricted"`
-		IsBot             bool         `json:"is_bot"`
-		IsAppUser         bool         `json:"is_app_user"`
-		IsEmailConfirmed  bool         `json:"is_email_confirmed"`
-		Updated           int          `json:"updated"`
-	}
-	SlackMembersResponse struct {
-		OK       bool          `json:"ok"`
-		Error    string        `json:"error"`
-		Members  []SlackMember `json:"members"`
-		CacheTS  int           `json:"cache_ts"`
-		Metadata struct {
-			NextCursor string `json:"next_cursor"`
-		} `json:"response_metadata"`
-	}
+	"github.com/triax/hub/models"
 )
 
 func CronFetchSlackMembers(w http.ResponseWriter, req *http.Request) {
@@ -76,7 +32,7 @@ func CronFetchSlackMembers(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	slackres := new(SlackMembersResponse)
+	slackres := new(models.SlackMembersResponse)
 	if err := json.NewDecoder(res.Body).Decode(slackres); err != nil {
 		fmt.Println("[ERROR]", 4003, err)
 		w.WriteHeader(http.StatusInternalServerError)
@@ -89,7 +45,31 @@ func CronFetchSlackMembers(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 
+	client, err := datastore.NewClient(ctx, os.Getenv("GOOGLE_CLOUD_PROJECT"))
+	if err != nil {
+		fmt.Println("[ERROR]", 4005, slackres.Error)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	keys := []*datastore.Key{}
+	members := []models.Member{}
+	for _, m := range slackres.Members {
+		if m.IsBot || m.IsAppUser {
+			continue
+		}
+		keys = append(keys, datastore.NameKey("Member", m.ID, nil))
+		members = append(members, models.Member{Slack: m})
+	}
+
+	if _, err := client.PutMulti(ctx, keys, members); err != nil {
+		fmt.Println("[ERROR]", 4006, err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
 	marmoset.RenderJSON(w, http.StatusOK, marmoset.P{
-		"count": len(slackres.Members),
+		"message": "ok",
+		"count":   len(members),
 	})
 }
--- go.mod
+++ go.mod
@@ -1,9 +1,9 @@
 module github.com/triax/hub
 
-go 1.14
+go 1.15
 
 require (
+	cloud.google.com/go/datastore v1.5.0
 	github.com/otiai10/appyaml v0.0.0-20210625032121-1fe2f3423963
 	github.com/otiai10/marmoset v0.5.0
-	golang.org/x/net v0.0.0-20210614182718-04defd469f4e // indirect
 )
