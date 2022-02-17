a0c3216d9d02cd88cb04357b757e9d8cb954db76
Hiromu OCHIAI <otiai10@gmail.com>
2022-02-17 18:38:33 +0900

    Fix click event of equips/report page

--- client/pages/equips/report.tsx
+++ client/pages/equips/report.tsx
@@ -52,9 +52,9 @@ function EquipCard({ equip, selected, toggle }: { equip: Equip, selected: boolea
       className={`rounded-lg px-4 py-2 mb-6 ${card}`}
     >
       <input
+        onClick={() => toggle(equip.id)}
         className="mr-4 leading-tight w-6 h-6" type="checkbox"
         checked={selected}
-        disabled={true}
       />
       <span
         className="text-xl"
