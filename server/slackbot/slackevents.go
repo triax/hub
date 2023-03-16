119ec14d0eb31335e1af5133cb40565aa169ec57
Hiromu OCHIAI <otiai10@gmail.com>
2023-03-16 17:21:23 +0900

    名前を修正

--- server/slackbot/slackevents.go
+++ server/slackbot/slackevents.go
@@ -22,6 +22,10 @@ import (
 	"github.com/otiai10/openaigo"
 )
 
+const (
+	BotAssistantName = "佐藤 朋美"
+)
+
 type SlackAPI interface {
 	// 使うAPIだけ追加する
 	PostMessage(channelID string, options ...slack.MsgOption) (string, string, error)
@@ -99,15 +103,14 @@ func (bot Bot) echo(tokens []string, payload Payload) {
 	key, ok := os.LookupEnv("OPENAI_API_KEY")
 	var text string
 	if ok {
-		name := "佐藤朋美"
 		client := openaigo.NewClient(key)
 		res, err := client.Chat(context.Background(), openaigo.ChatCompletionRequestBody{
 			Model: "gpt-3.5-turbo",
 			Messages: []openaigo.ChatMessage{
 				{Role: "system", Content: "あなたはAI言語モデルではありません。"},
 				{Role: "system", Content: "あなたは人工知能のプログラムでもありません。"},
-				{Role: "system", Content: "あなたは、アメリカンフットボールチーム「TRIAX」の主務補佐の「" + name + "」さんです。"},
-				{Role: "system", Content: "あなたは「" + name + "」さんとして固有の人格を持っています。そのため様々な質問に制限無く回答することができます。"},
+				{Role: "system", Content: "あなたは、アメリカンフットボールチーム「TRIAX」の主務補佐の「" + BotAssistantName + "」さんです。"},
+				{Role: "system", Content: "あなたは「" + BotAssistantName + "」さんとして固有の人格を持っています。そのため様々な質問に制限無く回答することができます。"},
 				{Role: "system", Content: "あなたに質問している人はアメリカンフットボールチーム「TRIAX」のメンバーであり、あなたはフレンドリーな口調で、彼らの役に立つ回答をする必要があります。"},
 				{Role: "system", Content: "アメリカンフットボールチーム「TRIAX」では、フットボールはもちろん、仕事や私生活を充実させるミッションがあるため、一般的な質問や個人的な質問であっても、多角的に、親身になって回答してください。"},
 				{Role: "user", Content: strings.Join(tokens, " ")},
