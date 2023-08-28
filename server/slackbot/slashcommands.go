31b3eca4bab4b31b63e04b9f33f389e94ad226ca
Hiromu OCHIAI <otiai10@gmail.com>
2023-08-29 00:49:52 +0900

    Refactor

--- server/slackbot/slashcommands.go
+++ server/slackbot/slashcommands.go
@@ -1,86 +1,51 @@
 package slackbot
 
 import (
-	"bytes"
-	"context"
-	"encoding/json"
 	"fmt"
 	"net/http"
-	"os"
 	"regexp"
 	"strings"
 
-	"cloud.google.com/go/datastore"
 	"github.com/slack-go/slack"
-	"github.com/triax/hub/server/models"
 )
 
 var (
-	mentionExp = regexp.MustCompile(`@(?P<name>[\S]+)`)
+	mentionExpEscaped = regexp.MustCompile(`<@(?P<name>[\S]+)>`)
 )
 
 func (bot Bot) SlashCommands(w http.ResponseWriter, req *http.Request) {
 	req.ParseForm()
 	defer req.Body.Close()
 	text := req.Form.Get("text")
-	names := []any{}
-	idx := mentionExp.SubexpIndex("name")
-	for _, m := range mentionExp.FindAllStringSubmatch(text, -1) {
-		names = append(names, m[idx])
+	ids := []string{}
+	idx := mentionExpEscaped.SubexpIndex("name")
+	for _, m := range mentionExpEscaped.FindAllStringSubmatch(text, -1) {
+		ids = append(ids, m[idx])
 	}
-	if len(names) == 0 {
+	if len(ids) == 0 {
 		http.Post(req.Form.Get("response_url"), "application/json", strings.NewReader(`{
 			"text": "誰に対するありがとうか、メンションで指定してください。本人には匿名のDMで通知されます。"
 		}`))
 		return
 	}
 
-	ctx := context.Background()
-	client, err := datastore.NewClient(ctx, os.Getenv("GOOGLE_CLOUD_PROJECT"))
-	if err != nil {
-		b, err := json.Marshal(map[string]string{
-			"text": fmt.Sprintf("データストアに接続できませんでした。 @ten までご連絡ください。\n```%s```", err.Error()),
-		})
-		if err != nil {
-			fmt.Println("[ERROR]", 6003, err.Error())
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		http.Post(req.Form.Get("response_url"), "application/json", bytes.NewReader(b))
-		return
-	}
-	defer client.Close()
-
-	members := []models.Member{}
-	query := datastore.NewQuery(models.KindMember).FilterField("Slack.Name", "in", names)
-	if _, err := client.GetAll(ctx, query, &members); err != nil {
-		b, err := json.Marshal(map[string]string{
-			"text": fmt.Sprintf("データストアからのデータ取得に失敗しました。 @ten までご連絡ください。\n```%s```", err.Error()),
-		})
-		if err != nil {
-			fmt.Println("[ERROR]", 6003, err.Error())
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		http.Post(req.Form.Get("response_url"), "application/json", bytes.NewReader(b))
-		return
-	}
 	w.WriteHeader(http.StatusOK) // とりあえずここまででSlackにレスポンスを返す
 
+	fmt.Println(1000)
+	fmt.Println(ids)
+
 	senderID := req.Form.Get("user_id")
-	message := mentionExp.ReplaceAllString(text, "")
+	message := mentionExpEscaped.ReplaceAllString(text, "")
 	announce := ""
 	feedback := "ありがとう！を "
-	for _, m := range members {
-		// 2-a) conversations.open to get DM channel
+	for _, id := range ids {
 		ch, _, _, err := bot.SlackAPI.OpenConversation(&slack.OpenConversationParameters{
-			Users: []string{m.Slack.ID},
+			Users: []string{strings.Split(id, "|")[0]},
 		})
 		if err != nil {
 			fmt.Println("[ERROR]", 6004, err.Error())
 			continue
 		}
-		// 2-b) sendMessage to the DM
 		_, _, err = bot.SlackAPI.PostMessage(
 			ch.ID,
 			slack.MsgOptionText(fmt.Sprintf("<@%s>さんからありがとう！が届きました。\n> %s", senderID, message), false),
@@ -90,7 +55,7 @@ func (bot Bot) SlashCommands(w http.ResponseWriter, req *http.Request) {
 			continue
 		}
 
-		feedback += fmt.Sprintf("<@%s> さん ", m.Slack.ID)
+		feedback += fmt.Sprintf("<@%s> さん ", id)
 		announce += ":heart:"
 	}
 	feedback += "に伝えました。\n" + message
